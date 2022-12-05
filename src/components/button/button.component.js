84a83cd15ddf160723c8332b19525920fb8de659
Chris Knudsvig <knudcb28@gmail.com>
2022-12-05 16:27:37 -0600

    added styled components for buttons

--- src/components/button/button.component.js
+++ src/components/button/button.component.js
@@ -1,4 +1,4 @@
-import { BaseButton, GoogleSignInButton, InvertedButton } from './button-styles.js';
+import { BaseButton, GoogleSignInButton, InvertedButton } from './button-styles';
 
 export const BUTTON_TYPE_CLASSES = {
     base: 'base',
@@ -11,13 +11,11 @@ const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) =>
         [BUTTON_TYPE_CLASSES.base]: BaseButton,
         [BUTTON_TYPE_CLASSES.google]: GoogleSignInButton,
         [BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
-
     }[buttonType]);
 
 
 const Button = ({ children, buttonType, ...otherProps }) => {
     const CustomButton = getButton(buttonType);
-
     return <CustomButton {...otherProps}>{ children }</CustomButton>;
 };
 
--- src/components/sign-in-form/sign-in-form.component.js
+++ src/components/sign-in-form/sign-in-form.component.js
@@ -79,8 +79,8 @@ const SignInForm = () => {
         <div className='buttons-container'>
           <Button type='submit'>Sign In</Button>
           <Button 
-            type='button' 
             buttonType={BUTTON_TYPE_CLASSES.google} 
+            type='button' 
             onClick={signInWithGoogle}
           >
             Sign In With Google
